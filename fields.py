076b5512ce3915f4a6d3e818be967054bfdb14f2
VPavelko <shadows0breath@gmail.com>
2023-12-17 18:41:48 +0200

    add fields implementation

---
+++ fields.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+from re import match
+from common import map_err_message
+
+class Field:
+    @map_err_message(TypeError, "Value must be a string")
+    def __init__(self, value):
+        if type(value) != str:
+            raise TypeError()
+
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    @map_err_message(ValueError, "Name must be at least 1 character long")
+    def __init__(self, value):
+        if len(value) < 1:
+            raise ValueError()
+
+        super().__init__(value)
+
+class Phone(Field):
+    @map_err_message(ValueError, "Invalid phone format. Expected: 10 digits")
+    def __init__(self, value):
+        # if match(r"^\d{10}$", value) is None:
+            # raise ValueError()
+
+        super().__init__(value)
+
+class Birthday(Field):
+    @map_err_message(ValueError, "Invalid birthday format. Expected: DD.MM.YYYY")
+    def __init__(self, value):
+        value = datetime.strptime(value, "%d.%m.%Y")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+
+    def phones_str(self):
+        return '\n'.join(f"- {p.value}" for p in self.phones)
+
+    def __str__(self):
+        name = self.name.value
+        phones = self.phones_str()
+
+        return f"name: {name}\nphones:\n{phones}"
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+
+    def edit_phone(self, old_phone, new_phone):
+        phone = self.find_phone(old_phone)
+
+        if phone is None:
+            raise ValueError("Phone not found")
+
+        phone.value = new_phone
